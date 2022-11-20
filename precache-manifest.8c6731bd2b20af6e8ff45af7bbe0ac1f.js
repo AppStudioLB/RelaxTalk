25298a89e52f7dfee3ea8ffed2457d7b55d53828
AppStudioLB <63586073+AppStudioLB@users.noreply.github.com>
2022-11-20 16:36:48 +0900

    Updates

---
+++ precache-manifest.8c6731bd2b20af6e8ff45af7bbe0ac1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260cb818d00e9229e41448b90f44e4d3",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "eba907fc300f967d5f0b",
+    "url": "/nwitter/static/css/main.13b90e90.chunk.css"
+  },
+  {
+    "revision": "bcb634821819a6295a0c",
+    "url": "/nwitter/static/js/2.d81efbf0.chunk.js"
+  },
+  {
+    "revision": "ff452becbfcd14efe470e758301a1f2a",
+    "url": "/nwitter/static/js/2.d81efbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba907fc300f967d5f0b",
+    "url": "/nwitter/static/js/main.be74afa1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/nwitter/static/js/main.be74afa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125d1d768f4e9614d81a",
+    "url": "/nwitter/static/js/runtime-main.3a662223.js"
+  }
+]);
